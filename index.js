165e38dd6c71501056c9928c6bc5cbc18cfc3007
John Loberger <johnloberger@gmail.com>
2020-03-23 02:18:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var greeting = "Hello, everybody!"
-var greetSpecialGuest = "Hello, " + specialGuest + "!"
-var specialGuest = "Niel deGrasse Tyson"
-var conversation = "'Let\'s talk about " + topic + "."
-var topic = "test"
+var greeting = "";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "" + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `${topic}`;
